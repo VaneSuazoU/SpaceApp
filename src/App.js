4f12bc32963419d9fd6c7312c992e76f8aa0856c
Rodolfo Montecino <vsuazo.93@gmail.com>
2021-07-19 16:30:22 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+const App = () => {
+  return (
+    <React.Fragment>
+      <h1>Hola</h1>
+    </React.Fragment>
+  );
+};
+
+export default App;
