573d28942f3e6ce5ff7c5ea5497a33455ff8da21
majolagos <mlagos.r90@gmail.com>
2021-07-22 01:31:15 -0400

    solution input delete

--- src/components/Attendees.jsx
+++ src/components/Attendees.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 
 const Attendees = () => {
- 
-
+  let contador = 0;
   const addAttendees = (e) => {
     e.preventDefault();
-     const div = document.querySelector("#attendeesDiv");
+    contador++;
+    const id = "a" + contador;
+    const div = document.querySelector("#attendeesDiv");
     const col1 = document.createElement("div");
     col1.className = "col-3";
+    col1.id = id;
     const input = document.createElement("input");
     input.className = "form-control";
     input.type = "email";
@@ -16,6 +18,7 @@ const Attendees = () => {
 
     const col2 = document.createElement("div");
     col2.className = "col-3";
+    col2.id = id;
     const input2 = document.createElement("input");
     input2.className = "form-control";
     input2.type = "text";
@@ -23,21 +26,30 @@ const Attendees = () => {
     col2.appendChild(input2);
 
     const btnDelete = document.createElement("button");
+    btnDelete.id = id;
     btnDelete.className = "btn btnClose popover-test";
-    btnDelete.onclick = (e) => deleteAttendees(e);
+    btnDelete.onclick = (e) => deleteAttendees(e, id);
     btnDelete.textContent = "X";
 
     div.appendChild(col1);
     div.appendChild(col2);
     div.appendChild(btnDelete);
   };
-  const deleteAttendees = (e) => {
+  const deleteAttendees = (e, id) => {
     e.preventDefault();
+
+    const algo = document.querySelectorAll("#" + id);
+    for (let i = 0; i < algo.length; i++) {
+      console.log(algo[i]);
+      const div = document.getElementById("attendeesDiv");
+      div.removeChild(algo[i]);
+      // remove(algo[i]);
+    }
   };
-  // const limpiar = (e) => {
-  //   e.preventDefault();
-  //   div.innerHTML = " ";
-  // };
+  const clean = (e) => {
+    e.preventDefault();
+    document.querySelector("#attendeesDiv").innerHTML = "";
+  };
 
   return (
     <div
@@ -90,12 +102,12 @@ const Attendees = () => {
             >
               +
             </button>
-            {/* <button
+            <button
               className="btn btn-warning popover-test"
-              onClick={(e) => limpiar(e)}
+              onClick={(e) => clean(e)}
             >
               Limpiar
-            </button> */}
+            </button>
             <button className="btnPlus" data-dismiss="modal">
               Listo
             </button>
